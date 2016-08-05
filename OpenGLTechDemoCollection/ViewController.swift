a4442890555281f8b9358670d27d66199d3a2292
Andreas Umbach <umbach@gmail.com>
2016-08-05 12:22:20 +0200

    Initial Commit

---
+++ OpenGLTechDemoCollection/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OpenGLTechDemoCollection
+//
+//  Created by Andreas Umbach on 05.08.2016.
+//  Copyright © 2016 Andreas Umbach. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
